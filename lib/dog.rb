774f4ce14d3e7eb277827050e19aaf346439350b
Daniel <dannyblaze469@gmail.com>
2021-08-17 13:14:50 -0400

    commit tree 8c1089f815ff9fc1961c2e66ebe8be87a99ced31

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,32 @@
-class Dog
+class Dog < ActiveRecord::Base
+
+    def self.create
+        
+    end
+
+    def self.save
+
+    end
+
+    def self.update
+
+    end
+
+    def self.find_or_create_by
+
+    end
+
+    def self.find_by_name
+
+    end
+
+    def self.find_by_id
+
+    end
+
+    def initialize(options = {})
+        @name = options[:name]
+        @breed = options[:breed]
+    end
+
 end
